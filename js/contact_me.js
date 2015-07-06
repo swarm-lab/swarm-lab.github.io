4d6d12125127db0d50b0acd0f42d0ae682b749d5
unknown <garnier@garnier-X1C.njitdm.campus.njit.edu>
2015-07-06 13:53:26 -0400

    Corrected errors in mailing code for sending contact form.

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,7 +10,7 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
+            var website = $("input#website").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -22,7 +22,7 @@ $(function() {
                 type: "POST",
                 data: {
                     name: name,
-                    phone: phone,
+                    website: website,
                     email: email,
                     message: message
                 },
