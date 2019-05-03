555734354497514ecfef3ace71a8e2c2426a8a2c
Simon Garnier <garnier@njit.edu>
2019-05-03 18:54:38 -0400

    Updates website

--- scripts/scholar.R
+++ scripts/scholar.R
@@ -1,18 +1,75 @@
-library(rvest)
-library(knitr)
+if (!require(rvest)) {
+  install.packages("rvest", repos="http://cran.us.r-project.org")
+  require(rvest)
+}
 
-scholar_tab <- "https://scholar.google.com/citations?user=4MNHWX8AAAAJ" %>%
-  read_html(.) %>%
+if (!require(dplyr)) {
+  install.packages("dplyr", repos="http://cran.us.r-project.org")
+  require(dplyr)
+}
+
+if (!require(knitr)) {
+  install.packages("knitr", repos="http://cran.us.r-project.org")
+  require(knitr)
+}
+
+if (!require(stringr)) {
+  install.packages("stringr", repos="http://cran.us.r-project.org")
+  require(stringr)
+}
+
+if (!require(ggplot2)) {
+  install.packages("ggplot2", repos="http://cran.us.r-project.org")
+  require(ggplot2)
+}
+
+### 
+
+scholar_html <- read_html("https://scholar.google.com/citations?user=4MNHWX8AAAAJ")
+
+scholar_tab <- scholar_html %>%
   html_nodes(xpath='//*[@id="gsc_rsb_st"]') %>%
   html_table(fill = TRUE)
 names(scholar_tab[[1]])[1] <- paste0("As of ", format(Sys.time(), "%B %d, %Y"))
 scholar_tab <- strsplit(kable(scholar_tab), "\n")[[1]]
 
 scholar_page <- readLines("publications/index.md")
-start <- which(scholar_page == "#### Google Scholar Report")
+# start <- which(scholar_page == "#### Google Scholar Report")
+start <- grep("scholar.png", scholar_page)
 end <- min(which(scholar_page == "---")[which(scholar_page == "---") > start])
 scholar_page <- scholar_page[-(start + 1):-(end - 1)]
 
 out <- c(scholar_page[1:start], "", "{:.scholar}", scholar_tab, "", scholar_page[(start + 1):length(scholar_page)])
 
 writeLines(out, "publications/index.md")
+
+### 
+
+scholar_df <- data.frame(years = scholar_html %>%
+                           html_nodes(xpath='//*[@class="gsc_g_t"]') %>%
+                           as.character(.) %>%
+                           str_extract(., "(?<=>).+(?=<)") %>%
+                           as.numeric,
+                         citations = scholar_html %>%
+                           html_nodes(xpath='//*[@class="gsc_g_al"]') %>%
+                           as.character(.) %>%
+                           str_extract(., "(?<=>).+(?=<)") %>%
+                           as.numeric()) %>%
+  mutate(., cum_citations = cumsum(citations)) 
+
+png("img/posts/publications/scholar.png", width = 768, height = 250)
+ggplot(scholar_df, aes(years)) +
+  geom_col(aes(y = cum_citations), fill = "#b4133b") +
+  geom_col(aes(y = cum_citations - citations), fill = "#008ea5") +
+  geom_text(aes(y = cum_citations, label = paste0("+", citations)), 
+            position = position_dodge(width = 0.9), vjust = -0.75,
+            fontface = "bold", size = 5) +
+  xlab(NULL)+ ylab(NULL) +
+  scale_y_continuous(limits = c(0, 1.1 * max(scholar_df$cum_citations)),
+                     sec.axis = sec_axis(~.)) +
+  theme_minimal(base_size = 18) +
+  theme(text = element_text(face = "bold"),
+        plot.background = element_rect(fill = "#ececec", color = "#cccccc"),
+        panel.grid.major = element_line(color = "#fafafa"),
+        panel.grid.minor = element_line(color = "#fafafa"))
+dev.off()
